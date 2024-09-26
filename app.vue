4e58840d8a176e1a58c267a3169a5f35483f39bc
tawan <thiago_tawan@outlook.com>
2024-09-26 07:11:43 -0300

    first

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: "Inter";
+  background: #624f25;
+  color: rgb(233, 232, 232);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div
+      class="w-full border-b border-gray-300 flex justify-center items-center p-2"
+    >
+      <h2>Eu e Quem eu Amo</h2>
+    </div>
+
+    <div class="mt-48 flex items-center flex-col h-screen">
+      <h3 class="text-sm text-center w-48">
+        Se declarar é uma das melhores formas do amor.
+      </h3>
+      <img
+        src="https://i.pinimg.com/736x/cb/21/c9/cb21c9b92e4fc88f4c0309bbab1c8a0a.jpg"
+        class="w-44 h-44 rounded-lg object-cover border-8 mt-4"
+      />
+      <p class="text-[12px] mt-1 font-mono">Amor da minha vida</p>
+
+      <button class="font-bold mt-8 bg-white px-8 py-2 rounded-lg text-black">
+        Começar!
+      </button>
+    </div>
+  </div>
+</template>
